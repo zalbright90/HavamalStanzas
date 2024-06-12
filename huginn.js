957b54a0820a02d19940a23a289c847edc13af9d
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-06-12 00:06:58 -0700

    fix for js
    Having issues with the stanza not loading, moved the end of stanzas ending code to the top of the commented stanzas

--- huginn.js
+++ huginn.js
@@ -220,6 +220,7 @@ const stanzas = [
         norse: "Erindi 55: Meðalsnotr skyli manna hverr, æva til snotr sé; því at snotrs manns hjarta verðr sjaldan glatt, ef sá er alsnotr, er á.",
         english: "Stanza 55: Middling wise must every man be, he should not be overwise ever, for the heart of a wise man hardly ever becomes glad, if its owner is wise in everything."
     }
+];
     /* Need to finish filling in English Translation.
     {
         norse: "Erindi 56: Meðalsnotr skyli manna hverr, æva til snotr sé; örlög sín viti engi fyrir, þeim er sorgalausastr sefi.",
@@ -665,7 +666,7 @@ const stanzas = [
         english: "Stanza 165:"
     }
 After English translation, store all on db and reconfigure*/        
-];
+
 
 let currentLanguage = 'norse';
 
